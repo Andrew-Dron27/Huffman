68d2334cd8659be93779cc642b7497ad8a778a91
Andrew Dron <adron@FoShizzle>
2022-10-27 15:21:09 -0600

    Reading file and added test files

--- huffman.cpp
+++ huffman.cpp
@@ -3,10 +3,11 @@
 using namespace std;
 Huffman::Huffman(ifstream inFile)
 {
+    
     unordered_map<char, int> freq;
     while (inFile >> noskipws >> ch) 
     {
-        if(count(freq[ch]) == 0)
+        if(freq.count(ch))//this is super slow
         {
             freq[ch] = 0;
         }
--- main.cpp
+++ main.cpp
@@ -1,17 +1,28 @@
 #include <iostream>
 #include <fstream>
+#include <filesystem>
+#include "huffman.h"
 using namespace std;
 
 
 int main(int argc, char** argv)
 {
-    std::cout << argv[1] << std::endl;
+    if(argc == 1)
+    {
+        cerr << "Please enter in a file as second argument";
+        exit(1);
+    }
+    else if(argc > 2)
+    {
+        cerr << "Too many arguments";
+        exit(1);
+    }
+    string fileName = std::filesystem::current_path().string() + "/Test_Files/" +  argv[1];
     ifstream inFile;
-    infile.open(argv[1]);
+    inFile.open(fileName);
     if (!inFile)
     {
-        cerr << "Unable to open file datafile.txt";
+        cerr << "Unable to open file textfile";
         exit(1);   // call system to stop
     }
-    
 }
